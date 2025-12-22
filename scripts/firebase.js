7f6db21923df13840a9e51e1caed2d246ea78c8f
Shriya Avhad <avhadshriya@gmail.com>
2025-12-23 00:35:09 +0530

    ..

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,11 +1,13 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDyG9y4j-ofpjH2J648V_FqdlWbZ-bSobA",
-  authDomain: "lilacstore-2305.firebaseapp.com",
-  projectId: "lilacstore-2305",
-  appId: "1:698524267252:web:a23fdcaa1b6a0a2d2aa4a1",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
